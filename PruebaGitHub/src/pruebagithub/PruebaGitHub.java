b7bf302b8514216c2f92293206249358640b12f2
KatarzynaMonikaRaune <KatarzynaMonikaRaune@2DAMB-15>
2022-10-21 12:16:29 +0200

    Print tortitas

---
+++ PruebaGitHub/src/pruebagithub/PruebaGitHub.java
@@ -0,0 +1,12 @@
+
+package pruebagithub;
+
+public class PruebaGitHub {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Quiero tortitas. :<");
+    }
+    
+    
+}
